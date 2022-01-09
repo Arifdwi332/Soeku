18b1524ed02c4625ec877500c001c8d3a080073c
ariefdwi332 <ariefdwi332@gmail.com>
2022-01-09 11:36:51 +0700

    comit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderSoeku />
+    <HeroSoeku />
+    <ProductSoeku />
+    <InstaSoeku />
+    <PartnerSoeku />
+    <FooterSoeku />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSoeku from '@/components/HeaderSoeku.vue' 
+import HeroSoeku from '@/components/HeroSoeku.vue' 
+import ProductSoeku from '@/components/ProductSoeku.vue' 
+import InstaSoeku from '@/components/InstaSoeku.vue' 
+import PartnerSoeku from '@/components/PartnerSoeku.vue' 
+import FooterSoeku from '@/components/FooterSoeku.vue' 
+
+export default {
+  name: 'Home',
+  components: {
+    //HelloWorld
+    HeaderSoeku,
+    HeroSoeku,
+    ProductSoeku,
+    InstaSoeku,
+    PartnerSoeku,
+    FooterSoeku
+  }
+}
+</script>
